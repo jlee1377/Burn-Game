fac870eb01790f0ddab7f1209e55115555e52b7e
Joe Lee <joseph.joe93@gmail.com>
2019-04-18 22:08:38 -0700

    init files
    set up deck file types and game

---
+++ Burn.py
@@ -0,0 +1,26 @@
+import deck as newdeck
+import random as rand
+
+deck = newdeck.listDeck
+userHand = {}
+cpuHand = {}
+userFieldUp = {}
+cpuFieldUp = {}
+userFieldDown = {}
+cpuFieldDown = {}
+
+fieldPile = {}
+# deal(user)
+# deal(cpu)
+# def deal(self):
+#     self.append
+
+# print(len(deck))
+print(deck.pop())
+# print(len(deck))
+
+print(deck.pop())
+print(deck.pop())
+print(deck.pop())
+
+# TODO find way to make numbers match (S2 AND D2)
